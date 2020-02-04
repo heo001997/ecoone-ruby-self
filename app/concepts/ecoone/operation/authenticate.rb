93ef9bc19e723d7a9d2ee55c74167244c0e712c4
tunt <tunt@ecotekcorp.com>
2020-02-04 09:26:47 +0700

    ecoone -> operation -> understanding codes in filter_and_sort_model.rb

---
+++ app/concepts/ecoone/operation/authenticate.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+class Ecoone::Operation::Authentication < BaseOperation
+  step :verify_token_present
+  step :verify_token
+  step :set_current_user
+  step :verify_current_user_role_active
+
+  raise :set_unauthenticated, fail_fast: true
+
+  def verify_token_present(_options, request:, **)
+    # Fail unless Auth header is set
+    return Railway.fail! if request.headers['Authorization'].nil?
+
+    Railway.pass!
+  end
+
+  # Verify user authentication token
+  def verify_token(options, request:, **)
+    token = request.headers['Authorization'].gsub(/Bearer /, '')
+    Railway.fail! if token.empty? || token.lenght < 10
+
+    options[:token] = token
+    Railway.pass!
+  end
+
+  def set_current_user(options, token:, **)
+    result = User::Operation::FindByToken.call(
+      params: {
+        token_id: token
+      },
+      model_class: User
+    )
+    return Railway.fail! if  result.failure?
+
+    options[:current_user] = result[:model]
+    Railway.pass!
+  end
+
+  # Verify that user has a currently active role otherwise unauthenticate
+  def verify_current_user_role(_options, current_user:, **)
+    Rails.logger.info("Authentication of user #{current_user.username} failed because of inactive role")
+    return Railway.fail! unless current_user.user_role.active?
+
+    Railway.pass!
+  end
+
+  def set_unauthenticated(options, **)
+    options[:errors] << {
+      code: 'user.unauthenticated',
+      message: 'Unauthenticated'
+    }
+    options[:http_status] = HttpStatus::UNAUTHENTICATED
+  end
+end
--- config/initializers/http_status.rb
+++ config/initializers/http_status.rb
@@ -1,1 +1,9 @@
-#wtf
+#wtf self definition
+class HttpStatus
+  SUCCESS           = 200
+  CREATED           = 201
+  UNAUTHENTICATED   = 401
+  FORBIDDEN         = 403
+  NOT_FOUND         = 404
+  INTERNAL_ERROR    = 500
+end
